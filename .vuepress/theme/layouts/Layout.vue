f2a3f6f2bd6882a2393388e869dd3882f703e6c2
Ace <ace963ace@gmail.com>
2020-05-25 11:41:31 +0800

    `

--- .vuepress/theme/layouts/Layout.vue
+++ .vuepress/theme/layouts/Layout.vue
@@ -4,6 +4,10 @@
     <Content v-else />
   </div>
 </template>
+import FrontmatterPagination from '@theme/layouts/FrontmatterPagination.vue'
 export default {
+    components: {
+      FrontmatterPagination
+  },
 };
 <style lang="stylus" scoped></style>
